5eeb6740bf457b169b78dd5b8fa26192a6c85a0d
Reinaldo Portocarrero <juniorportom@MacBook-Pro-de-Reinaldo.local>
2018-02-10 17:56:29 -0500

    Metodo de registro de usuario en el API

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,6 +2,8 @@
 
 var User = require('../models/user');
 
+var bcrypt = require('bcrypt-nodejs');
+
 function home (req, res){
 	res.status(200).send({
 		message: 'Hola mundo desde el servidor de NodeJS'
@@ -15,7 +17,48 @@ function pruebas(req, res){
 	});
 }
 
+function saveUser(req, res){
+	var params = req.body;
+	var user = new User();
+
+	if(params.name && params.surname && params.nick
+		&& params.email && params.password){
+		user.name = params.name;
+		user.surname = params.surname;
+		user.nick = params.nick;
+		user.email = params.email;
+		user.role = 'ROLE_USER';
+		user.image = null;
+
+		bcrypt.hash(params.password, null, null, (error, hash) =>{
+			user.password = hash;
+
+			user.save((error, userStored) => {
+				if(error) return res.status(500).send({message: 'Error al guardar el usuario'});
+
+				if(userStored){
+					res.status(200).send({
+						user: userStored
+					});
+				}else{
+					res.status(404).send({
+						message: 'No se ha registrado el usuario'
+					});
+				}
+			});
+
+		})
+
+	}else
+	{
+		res.status(200).send({
+			message: 'Envia todos los campos necesarios!!'
+		});
+	}
+}
+
 module.exports = {
 	home,
-	pruebas
+	pruebas,
+	saveUser
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -7,6 +7,7 @@ var api = express.Router();
 
 api.get('/home', UserController.home);
 api.get('/pruebas', UserController.pruebas);
+api.post('/register', UserController.saveUser);
 
 module.exports = api;
 
