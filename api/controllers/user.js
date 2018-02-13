17588cbbe9546796cc90621de0c5c7b65e792968
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-02-12 20:10:31 -0500

    Metodo de generacion y uso de token de autenticacion

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,6 +4,8 @@ var User = require('../models/user');
 
 var bcrypt = require('bcrypt-nodejs');
 
+var jwt = require('../services/jwt');
+
 function home (req, res){
 	res.status(200).send({
 		message: 'Hola mundo desde el servidor de NodeJS'
@@ -85,9 +87,18 @@ function loginUser(req, res){
 		if(user){
 			bcrypt.compare(password, user.password, (error, check) =>{
 				if(check){
-					//devolver datos de usurio
-					user.password = undefined; // Eliminar propiedades de la respuesta
-					res.status(200).send({user});
+
+					if(params.gettoken){
+						//Generar y Devolver token
+						return res.status(200).send({
+							token: jwt.createToken(user)
+						});
+
+					}else{
+						//devolver datos de usurio
+						user.password = undefined; // Eliminar propiedades de la respuesta
+						res.status(200).send({user});
+					}					
 				}else{
 					res.status(404).send({message: 'El usuario no se ha podido identificar'});
 				}
