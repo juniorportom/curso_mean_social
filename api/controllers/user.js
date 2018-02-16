3444e3a2131c1ffd430d3972ed513730520d5e01
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-02-15 20:47:01 -0500

    Actualizar datos de usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -152,12 +152,37 @@ function getUsers(req, res){
 	});
 }
 
+// Edición de datos de usuario
+function updateUser(req, res){
+	var userId = req.params.id;
+	var update = req.body;
+
+	// Borrar propiedad password
+	delete update.password;
+	if(userId != req.user.sub)
+		return  res.status(500).send({
+			message: 'No tienes permiso para actualizar los datos del usuario'
+		});
+
+	User.findByIdAndUpdate(userId, update, {new: true} ,(error, userUpdated)=>{
+		if(error) return res.status(500).send({message: 'Error en la petición'});
+
+		if(!userUpdated) return res.status(404).sebd({message: 'No se ha podido actualizar el usuario'});
+
+		return res.status(200).send({user:userUpdated});
+	});
+}
+
+// Subir archivos de imagen/avatar de usuario
+
+
 module.exports = {
 	home,
 	pruebas,
 	saveUser,
 	loginUser,
 	getUser,
-	getUsers
+	getUsers,
+	updateUser
 }
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -13,6 +13,7 @@ api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 api.get('/user/:id', mdAuth.ensureAuth, UserController.getUser);
 api.get('/users/:page?', mdAuth.ensureAuth, UserController.getUsers);
+api.put('/update-user/:id', mdAuth.ensureAuth, UserController.updateUser);
 
 module.exports = api;
 
