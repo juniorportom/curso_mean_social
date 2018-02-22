7e99a9a930f1f37b9c60fbbfc1ca1aee647da149
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-02-21 19:57:21 -0500

    Metodos de listas de seguimiento de entidad follows

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -71,11 +71,64 @@ function getFollowingUsers(req, res){
 	})
 }
 
+function getFollowedUsers(req, res){
+	var userId = req.user.sub;
+	var page = 1;
+	var itemsPerPage = 4;
+
+	if(req.params.id && req.params.page){
+		userId = req.params.id;
+	}
+
+	if(req.params.page){
+		page = req.params.page;
+	}else{
+		page = req.params.id;
+	}
+
+
+	Follow.find({followed: userId}).populate('user followed').paginate(page, itemsPerPage, (error, follows, total)=>{
+		if(error) return res.status(500).send({message: 'Error en la petición'});
+
+		if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+
+		return res.status(200).send({
+			total: total,
+			pages: Math.ceil(total/itemsPerPage),
+			follows
+		});
+	})
+}
+
+
+//devolver usuarios que sigo
+function getMyFollows(req, res){
+	var userId = req.user.sub;
+
+	var find = Follow.find({user: userId});
+
+	if(req.params.followed){
+		find = Follow.find({followed: userId});
+	}
+
+
+	find.populate('user followed').exec((error, follows)=>{
+		if(error) return res.status(500).send({message: 'Error en la petición'});
+
+		if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningun usuario'});
+
+		return res.status(200).send({ follows });
+	})
+}
+
+
 module.exports = {
 	prueba,
 	saveFollow,
 	deleteFollow,
-	getFollowingUsers
+	getFollowingUsers,
+	getFollowedUsers,
+	getMyFollows
 }
 
 
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -12,5 +12,7 @@ api.get('/pruebas-follow', mdAuth.ensureAuth, FollowController.prueba);
 api.post('/follow', mdAuth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', mdAuth.ensureAuth, FollowController.deleteFollow);
 api.get('/following/:id?/:page?', mdAuth.ensureAuth, FollowController.getFollowingUsers);
+api.get('/followed/:id?/:page?', mdAuth.ensureAuth, FollowController.getFollowedUsers);
+api.get('/get-my-follows/:followed?', mdAuth.ensureAuth, FollowController.getMyFollows);
 
 module.exports = api;
