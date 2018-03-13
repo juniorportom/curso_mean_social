3ae9ad416ed121bc8221c82516e63dca1a261760
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-03-12 21:22:56 -0500

    Se crea proyecto de angular 5, se realizan configuraciones de librerias, y se agregan componentes de ejemplo

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders} from './app.routing';
+
+
+//Cargar comonentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
